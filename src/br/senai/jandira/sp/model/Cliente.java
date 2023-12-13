422d6f5e2849b87c9b82722988fdf8473e96da9a
Carolina Neponucena <cneponucena@gmail.com>
2023-12-13 12:30:11 -0300

    foi

---
+++ src/br/senai/jandira/sp/model/Cliente.java
@@ -0,0 +1,10 @@
+package br.senai.jandira.sp.model;
+
+public class Cliente {
+    private String nome, email;
+    private int clienteId = 0;
+    private int cpf, telefone;
+
+    public void cadastroCliente() {
+    }
+}
